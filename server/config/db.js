dae485af2d7c3bd8f7cd601366fde67950c68eb9
NikolaiKL17 <nikolajklusnikov6@gmail.com>
2026-03-29 21:50:36 +0500

    debug: выводим MONGODB_URI для Railway

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,9 @@ const dns = require('dns');
 // Использовать Google DNS для резолва SRV-записей MongoDB Atlas
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
+
 const connectDB = async () => {
+  console.log('MONGODB_URI:', process.env.MONGODB_URI);
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB подключена успешно');
